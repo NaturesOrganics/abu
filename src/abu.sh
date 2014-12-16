c6c7b5746efbd524464fb4d2035603589716ad5e
Phillip Smith <fukawi2@gmail.com>
2014-12-16 11:53:59 +1100

    relicense correctly

--- src/abu.sh
+++ src/abu.sh
@@ -3,18 +3,18 @@
 ###############################################################################
 # Copyright 2014 Natures Organics Pty Ltd
 #
-# This software is the property of Natures Organics Pty Ltd ("the owner") and
-# is protected by Australian and International copyright laws. This software is
-# licensed for internal use only. This software may not be reproduced,
-# duplicated, copied, sold, resold, or otherwise exploited for any purpose
-# without express written consent of the Owner.
-# Use of this software is governed by, construed and enforced in accordance
-# with the laws of Victoria, Australia. Disputes arising from your use of this
-# software are exclusively subject to the jurisdiction of the courts of
-# Victoria, Australia. This software may be accessed and used by users
-# throughout Australia and overseas.  The Owner makes no representations that
-# this software complies with the laws of any country outside Australia. You
-# are responsible for complying with the laws applicable in your location.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
 set -e
